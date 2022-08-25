9ff06fdafdcb90366cbdab00a3fa8a2b2562e67e
Michael Davey <michael.davey@robotica.ml>
2022-08-25 15:30:25 +0100

    Bump version number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="montreal-forced-aligner",
-    version="2.0.0rc5p1",
+    version="2.0.0rc6",
     description="The Montreal Forced Aligner is a command line utility for performing forced alignment of speech datasets using Kaldi",
     description_content_type="text/markdown",
     author=["roboticaml", "md84419"],
